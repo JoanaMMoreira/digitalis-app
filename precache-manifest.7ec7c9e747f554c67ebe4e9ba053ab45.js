69d9f1b611b66025f8658c4a27c04978ae74538b
Joana Moreira <31695437+JoanaMMoreira@users.noreply.github.com>
2019-02-02 21:23:50 +0000

    Updates

---
+++ precache-manifest.7ec7c9e747f554c67ebe4e9ba053ab45.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e5356fcf22d1740eab6c",
+    "url": "/digitalis-app/static/js/runtime~main.e5356fcf.js"
+  },
+  {
+    "revision": "a7898be68610488ea42a",
+    "url": "/digitalis-app/static/js/main.a7898be6.chunk.js"
+  },
+  {
+    "revision": "fd09364386ce5dee88d9",
+    "url": "/digitalis-app/static/js/1.fd093643.chunk.js"
+  },
+  {
+    "revision": "a7898be68610488ea42a",
+    "url": "/digitalis-app/static/css/main.edd3b2e9.chunk.css"
+  },
+  {
+    "revision": "fdf3708daeaa5c269b730d885c0b58a5",
+    "url": "/digitalis-app/index.html"
+  }
+];
